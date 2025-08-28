6bd97e08667ffae97a0e07c3b9d738e0535969ba
novanex-innovations <novanexinnovations3@gmail.com>
2025-08-29 02:44:31 +0500

    test file corrected

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sallarycheck/main.dart';
+import '../lib/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('SalaryCheckApp UI smoke test', (WidgetTester tester) async {
+    // Build the app and trigger a frame.
+    await tester.pumpWidget(SalaryCheckApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify the main title is present
+    expect(find.text('SalaryCheck'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify the subtitle is present
+    expect(find.textContaining('See what people'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify the search field is present
+    expect(find.byType(TextField), findsOneWidget);
+
+    // Verify the button is present
+    expect(find.text('Check Salary'), findsOneWidget);
   });
 }
